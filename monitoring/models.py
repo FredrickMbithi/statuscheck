6b60a15a2aedf71164ffa4bb90bc7d35e5c56446
FredrickMbithi <fredrickmbithi000@gmail.com>
2025-11-17 00:41:51 +0300

    Add installed apps for REST framework and Swagger documentation; define StatusCheckResult model

--- monitoring/models.py
+++ monitoring/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class StatusCheckResult(models.Model):
+    service_name = models.CharField(max_length=255)
+    success = models.BooleanField()
+    http_status_code = models.PositiveBigIntegerField(null=True, blank=True)
+    response_time_ms = models.FloatField(null=True, blank=True)
+    checked_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        status = "SUCCESS" if self.success else "FAIlED"
+        
